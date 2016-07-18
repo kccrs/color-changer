60b6db6027c776cc39a9c5e947da3fad0eee08bf
Casey Cross <hellocaseycross@gmail.com>
2016-07-18 16:55:01 -0600

    Change background color to random when random button is pushed and reset button changes to default white.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var randomButton = $('.random');
+var resetButton = $('.reset');
+
+$(function() {
+
+randomButton.on('click', function() {
+  var randomColor = 'rgb(' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ',' + (Math.floor(Math.random() * 255)) + ')';
+  $('body').css('background-color', randomColor);
+});
+
+resetButton.on('click', function() {
+  $('body').css('background-color', '#FFF')
+});
+
+
+
+
+
+
+
+
+});
